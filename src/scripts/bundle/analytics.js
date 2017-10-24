020bd277b95400bce5e2f613e8853da320d3d21e
Sam Deere <dev@colophonemes.com>
2017-10-24 15:39:27 -0700

    Remove outbound link tracking (was interfering with CMD+Click behaviour)

--- src/scripts/bundle/analytics.js
+++ src/scripts/bundle/analytics.js
@@ -1,35 +1,35 @@
 (function ($) {
   // track outbound link clicks
-  $('a').filter(function () {
-    return this.hostname && this.hostname !== window.location.hostname
-  }).on('click', function (event) {
-    var analytics = window.analytics
-    var linkURL = this.href
-    var linkTarget = this.target
-    if (linkTarget) {
-      // if we're opening in a new window, just send a track action
-      track(linkURL)
-    } else {
-      // if we're opening in the same window, send the track action, then open the page
-      event.preventDefault()
-      track(linkURL, function () {
-        window.location.href = linkURL
-      })
-    }
-    function track (linkURL, cb) {
-      // guard in case analytics isn't loaded
-      if (analytics.initialize) {
-        analytics.track('Clicked outbound link', {
-          category: 'Links',
-          label: linkURL
-        }, function () {
-          if (typeof cb === 'function') cb()
-        })
-      } else {
-        if (typeof cb === 'function') cb()
-      }
-    }
-  })
+  // $('a').filter(function () {
+  //   return this.hostname && this.hostname !== window.location.hostname
+  // }).on('click', function (event) {
+  //   var analytics = window.analytics
+  //   var linkURL = this.href
+  //   var linkTarget = this.target
+  //   if (linkTarget) {
+  //     // if we're opening in a new window, just send a track action
+  //     track(linkURL)
+  //   } else {
+  //     // if we're opening in the same window, send the track action, then open the page
+  //     event.preventDefault()
+  //     track(linkURL, function () {
+  //       window.location.href = linkURL
+  //     })
+  //   }
+  //   function track (linkURL, cb) {
+  //     // guard in case analytics isn't loaded
+  //     if (analytics.initialize) {
+  //       analytics.track('Clicked outbound link', {
+  //         category: 'Links',
+  //         label: linkURL
+  //       }, function () {
+  //         if (typeof cb === 'function') cb()
+  //       })
+  //     } else {
+  //       if (typeof cb === 'function') cb()
+  //     }
+  //   }
+  // })
 
   $("#mailchimp-signup-form").on('newsletter_signup',function(event,data){
     var analytics = window.analytics;
