81514a6b8c4d9e62dcb9ff0b65eb65d6646fab29
Sam Deere <dev@colophonemes.com>
2018-12-04 17:49:06 -0800

    Refactor scripts as async/await

--- src/scripts/app.bundle.js
+++ src/scripts/app.bundle.js
@@ -2,13 +2,13 @@
 
 	## app.bundle.js ##
 
-	This is the master file that organises all of the other scripts used by the site. 
+	This is the master file that organises all of the other scripts used by the site.
 
-	It uses Browserify to bundle scripts together into a single script, enabling control 
+	It uses Browserify to bundle scripts together into a single script, enabling control
 	over load order (as opposed to simply concatenating the directory), and keeps most
 	scripts out of the global scope.
 
-	All plugins are installed via NPM to the master node_modules directory. 
+	All plugins are installed via NPM to the master node_modules directory.
 
 	If a plugin is not available via NPM, or the NPM package's 'main' script is not the
 	script you want, you may need to set a 'browser' key in the master package.json.
@@ -22,70 +22,56 @@
 	handles the dependency resolution automatically.
 
 	See this post for more details:
-	http://blog.npmjs.org/post/112064849860/using-jquery-plugins-with-npm 
+	http://blog.npmjs.org/post/112064849860/using-jquery-plugins-with-npm
 
 */
 // cookies
-global.cookies = require('browser-cookies');
+global.cookies = require('browser-cookies')
 // lazysizes
-require('lazysizes');
-require('lazysizesRespImg');
-require('lazysizesBGSet');
+require('lazysizes')
+require('lazysizesRespImg')
+require('lazysizesBGSet')
 // bootstrap javascript plugins
-require('bootstrapDropdown');
-require('bootstrapCollapse');
-require('bootstrapTransition');
-require('bootstrapAffix');
-require('bootstrapScrollspy');
+require('bootstrapDropdown')
+require('bootstrapCollapse')
+require('bootstrapTransition')
+require('bootstrapAffix')
+require('bootstrapScrollspy')
 require('bootstrapModal')
 require('bootstrapTooltip')
 // third party bootstrap plugins
 // inject breakpoints into the global scope so we can use them elsewhere
 global.breakpoints = {
-    xs:480,
-    sm:768,
-    md:992,
-    lg:1200
-};
-
-// // GreenSock Animation Plugin
-// require('gsap-tweenlite');
-// require('gsap-timelinelite');
-// require('GSAPEasing');
-// require('GSAPCSSPlugin');
-// require('GSAPAttrPlugin');
-// require('GSAPScrollToPlugin');
-
-// // ScrollMagic
-// global.ScrollMagic = require('scrollmagic')
-// require('scrollmagicJQuery')
-// require('scrollmagicAnimationGSAP')
+  xs: 480,
+  sm: 768,
+  md: 992,
+  lg: 1200
+}
 
 // jQuery Plugins
-require('jqueryThrottleDebounce');
+require('throttle-debounce')
 // global.iFrameResize = require('iframeResizer');
 
 // form/data validation library
-global.validate = require('validate.js');
+global.validate = require('validate.js')
 
 // number formatting
-global.numeral = require('numeral');
+global.numeral = require('numeral')
 
 // exit intent plugin
 // global.ouibounce = require('ouibounce')
 
 // main app scripts
-require('main');
-require('analytics');
-require('newsletter');
+require('main')
+require('analytics')
+require('newsletter')
 
 // email descrambling
-const emailScramble = require('email-scramble');
-var links = document.querySelectorAll('[data-email-scramble]');
+const emailScramble = require('email-scramble')
+var links = document.querySelectorAll('[data-email-scramble]')
 for (var i = 0; i < links.length; i++) {
-	var link = links[i]
-	console.log(emailScramble.decode(link.innerHTML))
-	link.innerHTML = emailScramble.decode(link.innerHTML);
-	link.href = emailScramble.decode(link.href);
+  var link = links[i]
+  console.log(emailScramble.decode(link.innerHTML))
+  link.innerHTML = emailScramble.decode(link.innerHTML)
+  link.href = emailScramble.decode(link.href)
 }
-
--- tasks/scripts.js
+++ tasks/scripts.js
@@ -1,201 +1,138 @@
-var chalk = require('chalk');
-console.log(chalk.bold.cyan.inverse('Processing Javascript files'));
+// var chalk = require('chalk')
+// console.log(chalk.bold.cyan.inverse('Processing Javascript files'))
 
-var Promise = require('bluebird')
+const console = require('better-console')
+const fs = require('mz/fs')
 
-var fs = require('fs');
-Promise.promisifyAll(fs);
-fs.existsAsync = Promise.promisify (function exists(path, cb) {
-    fs.exists(path, function (exists) { cb(null, exists); });
-});
-
-var path = require('path')
-var rm = require('rimraf');
-var cp = require('ncp');
-var recursiveReaddir = require('recursive-readdir');
+const path = require('path')
+const { ncp } = require('ncp')
+const recursiveReaddir = require('recursive-readdir')
+const rimraf = require('rimraf')
 
 var Browserify = require('browserify')
-var uglify = Promise.promisifyAll(require('uglify-js'))
+var uglify = require('uglify-js').minify
 
-var filter = require('minimatch').filter;
+var filter = require('minimatch').filter
 
-var srcPath = path.join(__dirname,'..','src','scripts');
-var destPath = path.join(__dirname,'..','src','metalsmith','scripts')
-var builtPath = path.join(__dirname,'..','build','scripts')
+var SRC_PATH = path.join(__dirname, '..', 'src', 'scripts')
+var DEST_PATH = path.join(__dirname, '..', 'src', 'metalsmith', 'scripts')
+var BUILD_PATH = path.join(__dirname, '..', 'build', 'scripts')
 
+var scripts = []
 
+;(async () => {
+  try {
+    // remove destination path
+    console.info('Cleaning up...')
+    await rmdir(DEST_PATH)
+    await fs.mkdir(DEST_PATH)
+    // get source files
+    const files = await rreaddir(SRC_PATH)
+    // split files into includes and bundles
+    const includes = files.filter(filter('**/includes/**/*.js'))
+    const bundles = files.filter(filter('**/*.bundle.js'))
+    // check filename collisions
+    checkCollisions([...includes, ...bundles])
+    // copy source of include files
+    await Promise.all(includes.map(file => cp(
+      file,
+      path.join(DEST_PATH, path.basename(file))
+    )))
+    console.log('✓ Non-bundle files copied')
+    // copy minified versions of include files
+    const minifiedIncludes = await Promise.all(includes.map(minify))
+    console.log('✓ Non-bundle files minified')
+    // bundle files
+    console.info('Bundling...')
+    const minifiedBundles = await Promise.all(bundles.map(bundle))
+    console.log('✓ Bundles bundled')
+    // copy minified scripts and sourcemaps
+    const minifiedScripts = [...minifiedBundles, ...minifiedIncludes]
+    console.info('Copying minified scripts...')
+    await Promise.all(minifiedScripts.map(async script => {
+      await fs.writeFile(path.join(DEST_PATH, script.fileName), script.src)
+      await fs.writeFile(path.join(DEST_PATH, script.fileName + '.map'), script.map)
+    }))
+    console.log('✓ Copied minified scripts')
+    // copy to the build directory
+    if (await fs.exists(BUILD_PATH)) {
+      cp(DEST_PATH, BUILD_PATH)
+    }
+  } catch (err) {
+    console.error(err)
+  }
+})()
 
-var scripts = [];
-
-new Promise(function(resolve,reject){
-	rm(destPath,function(err){
-		if(err) reject(err);
-		resolve();
-	})
-})
-.then(function(){
-	return fs.mkdirAsync(destPath)
-	.catch(function(err){if(err.code !== 'EEXIST') throw err})
-})
-.then(function(){ return recursive(path.join(srcPath))})
-.then(function(files){
-	
-	// split files into includes and bundles
-	var includes = files.filter(filter('**/includes/**/*.js'));
-	var bundles  = files.filter(filter('**/*.bundle.js'));
-	
-	// error check to make sure we don't have any filename collisions
-	var fileNames = [];
-	[...includes,...bundles].map(function(file){
-		return path.basename(file);
-	}).forEach(function(fileName){
-		if(fileNames.indexOf(fileName)===-1){
-			fileNames.push(fileName);
-		} else {
-			console.error(chalk.red('Warning: multiple files exist with base name'),chalk.underline.bold.red(fileName))
-		}
-	})
-
-	// object to hold all async operations
-	var promises = [];
-
-	// copy regular files 
-	/*promises.push(
-		Promise.all(includes.map(function(file){
-			return copyFile(file,path.join(destPath,path.basename(file)));
-		}))
-		.then(function(){
-			console.log(chalk.green.bold('✓ Non-bundle files copied'));
-		})
-	)*/
-	// minify files
-	promises.push(
-		Promise.all(includes.map(function(file){
-			return minify(file)
-		}))
-		.then(function(minified){
-			scripts.push(...minified);
-			console.log(chalk.green.bold('✓ Non-bundle files minified'));
-		})
-	)
-	
-	// bundle files
-	promises.push(
-		Promise.all(bundles.map(function(file){
-				return bundle(file);
-		}))
-		.then(function(bundles){
-			scripts.push(...bundles);
-			console.log(chalk.green.bold('✓ All files bundled!'));
-		})
-
-	);
-	return Promise.all(promises);
-})
-.then(function(){
-	console.log(chalk.dim('Writing files to destination directory...'));
-	return Promise.all(scripts.map(function(script){
-		return Promise.all([
-			fs.writeFileAsync(path.join(destPath,script.fileName), script.src),
-			fs.writeFileAsync(path.join(destPath,script.fileName+'.map'), script.map)
-		])
-	}))
-	.then(function(){
-		console.log(chalk.green.bold('✓ All files written!'))
-	})
-})
-.then(function(cleaned){
-	return fs.existsAsync(builtPath)
-	.then(function(){
-		return new Promise(function(resolve,reject){
-			cp(destPath,builtPath,function(err){
-				if(err) reject(err);
-				resolve();
-			})
-		})
-	})
-	.then(function(){
-		console.log(chalk.green.bold('✓ Files copied to build directory!'));
-	})
-	.catch(function(err){
-		if(err.length === 1 && err[0].code==='ENOENT'){
-			// no build directory, don't bother doing anything
-		} else {
-			throw err;
-		}
-	})
-})
-.catch(function(err){
-	throw err;
-})
-
-
-function recursive(directory){
-	return new Promise(function(resolve,reject){
-		recursiveReaddir(directory, function(err,files){
-			if(err) reject(err);
-			resolve(files);
-		})
-	})
+async function rmdir (dir) {
+  return new Promise((resolve, reject) => {
+    rimraf(dir, (err) => {
+      console.log(`Removed ${dir}`)
+      if (err) return reject(new Error(err))
+      return resolve()
+    })
+  })
 }
 
-function minify(file){
-	return new Promise(function(resolve,reject){
-		
-		var fileName = path.basename(file).replace('.js','.min.js');
-		console.log(chalk.dim('+ Creating'),chalk.dim.underline(fileName));
-		try {
-			var minified = uglify.minify(file,{
-				outSourceMap: './'+fileName+'.map'
-			});
-		} catch(e){
-			reject(e);
-		}
-
-		console.log(chalk.green.dim('✓ Created'),chalk.dim.underline(fileName));
-		resolve({
-			src: Buffer.from(minified.code),
-			map: Buffer.from(minified.map),
-			fileName
-		})
-	})
-
+async function rreaddir (directory) {
+  return new Promise(function (resolve, reject) {
+    recursiveReaddir(directory, function (err, files) {
+      if (err) reject(err)
+      resolve(files)
+    })
+  })
 }
 
-function bundle(file){
-	return new Promise(function(resolve,reject){
-		var fileName = path.basename(file).replace('.bundle.js','.min.js');
-		console.log(chalk.dim('+ Creating'),chalk.dim.underline(fileName));
-	    // start browserify
-	    var browserify = new Browserify({debug:true});
-	    // add the entry file to the queue
-	    browserify.add(file)
-	    // add minifier / sourcemap generator
-	    browserify.plugin('minifyify', {map: './'+fileName+'.map', minify:true}); 
-	    // call the main bundle function
-	    browserify.bundle(function(err, src, map){
-	        if(err) reject(err);
-	        console.log(chalk.green.dim('✓ Created'),chalk.dim.underline(fileName));
-	        resolve({
-	        	src: src,
-	        	map: map,
-	        	fileName
-	        })
-	    })
-	})
+async function cp (source, destination) {
+  return new Promise((resolve, reject) => {
+    ncp(source, destination, (err) => {
+      if (err) return reject(Error(err))
+      return resolve()
+    })
+  })
 }
 
+function checkCollisions (files) {
+  // error check to make sure we don't have any filename collisions
+  files
+    .map(file => path.basename(file))
+    .reduce((acc, file) => {
+      if (acc.includes(file)) throw new Error(`There is more than one script with the name ${file}`)
+      acc.push(file)
+      return acc
+    }, [])
+}
 
-function copyFile(source, target) {
-    return new Promise(function(resolve, reject) {
-        var rd = fs.createReadStream(source);
-        rd.on('error', reject);
-        var wr = fs.createWriteStream(target);
-        wr.on('error', reject);
-        wr.on('finish', function(){
-	        console.log(chalk.green.dim('✓ Copied'),chalk.dim.underline(path.basename(target)));
-			resolve();
-        });
-        rd.pipe(wr);
-    });
+async function minify (file) {
+  const data = await fs.readFile(file).then(d => d.toString())
+  const fileName = path.basename(file).replace(/\.js$/, '.min.js')
+  const minified = uglify(data, {
+    sourceMap: {
+      filename: fileName,
+      url: `${fileName}.map`
+    }
+  })
+  return {
+    fileName,
+    src: minified.code,
+    map: minified.map
+  }
+}
+
+function bundle (file) {
+  return new Promise(function (resolve, reject) {
+    var fileName = path.basename(file).replace('.bundle.js', '.min.js')
+    console.info(`Creating ${fileName}`)
+    // start browserify
+    var browserify = new Browserify({ debug: true })
+    // add the entry file to the queue
+    browserify.add(file)
+    // add minifier / sourcemap generator
+    browserify.plugin('minifyify', { map: './' + fileName + '.map', minify: true })
+    // call the main bundle function
+    browserify.bundle(function (err, src, map) {
+      if (err) reject(err)
+      console.log(`Created ${fileName}`)
+      resolve({ src, map, fileName })
+    })
+  })
 }
