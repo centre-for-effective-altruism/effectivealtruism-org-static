6e039a4a8ce604fd80ddaefb080f9a335d3266dc
Sam Deere <dev@colophonemes.com>
2018-12-04 17:49:37 -0800

    Lint styles task

--- tasks/styles.js
+++ tasks/styles.js
@@ -27,66 +27,66 @@ new Promise(function (resolve, reject) {
     resolve()
   })
 })
-.then(function () {
-  return fs.mkdirAsync(destPath)
-  .catch(function (err) { if (err.code !== 'EEXIST') throw err })
-})
-.then(function () {
-  return sass.renderAsync({
-    file: srcFile,
-    outFile: 'app.css',
-    sourceMap: 'app.css.map'
+  .then(function () {
+    return fs.mkdirAsync(destPath)
+      .catch(function (err) { if (err.code !== 'EEXIST') throw err })
+  })
+  .then(function () {
+    return sass.renderAsync({
+      file: srcFile,
+      outFile: 'app.css',
+      sourceMap: 'app.css.map'
 
+    })
+      .then(function (css) {
+        console.log(chalk.green.bold('✓ Processed SASS into CSS'))
+        return css
+      })
   })
   .then(function (css) {
-  console.log(chalk.green.bold('✓ Processed SASS into CSS'))
-  return css
-})
-})
-.then(function (css) {
-  var clean = new cleanCSS({
-    sourceMap: css.map.toString(),
-    debug: true
-  })
-  Promise.promisifyAll(clean)
-  return clean.minifyAsync(css.css)
-  .then(function (cleaned) {
-  console.log(chalk.green.bold('✓ Cleaned CSS'))
-  return cleaned
-})
-})
-.then(function (cleaned) {
-  var files = {}
-  files['app.min.css'] = cleaned.styles.toString() + '\n/*# sourceMappingURL=./app.min.css.map */'
-  files['app.min.css.map'] = cleaned.sourceMap
-  return Promise.all(Object.keys(files).map(function (fileName) {
-    return fs.writeFileAsync(path.join(destPath, fileName), files[fileName])
-  }))
-  .then(function () {
-  console.log(chalk.green.bold('✓ All files saved!'))
-})
-})
-.then(function () {
-  return fs.existsAsync(builtPath)
-  .then(function () {
-  return new Promise(function (resolve, reject) {
-    cp(destPath, builtPath, function (err) {
-      if (err) reject(err)
-      resolve()
+    var clean = new cleanCSS({
+      sourceMap: css.map.toString(),
+      debug: true
     })
+    Promise.promisifyAll(clean)
+    return clean.minifyAsync(css.css)
+      .then(function (cleaned) {
+        console.log(chalk.green.bold('✓ Cleaned CSS'))
+        return cleaned
+      })
   })
-})
+  .then(function (cleaned) {
+    var files = {}
+    files['app.min.css'] = cleaned.styles.toString() + '\n/*# sourceMappingURL=./app.min.css.map */'
+    files['app.min.css.map'] = cleaned.sourceMap
+    return Promise.all(Object.keys(files).map(function (fileName) {
+      return fs.writeFileAsync(path.join(destPath, fileName), files[fileName])
+    }))
+      .then(function () {
+        console.log(chalk.green.bold('✓ All files saved!'))
+      })
+  })
   .then(function () {
-  console.log(chalk.green.bold('✓ Files copied to build directory!'))
-})
+    return fs.existsAsync(builtPath)
+      .then(function () {
+        return new Promise(function (resolve, reject) {
+          cp(destPath, builtPath, function (err) {
+            if (err) reject(err)
+            resolve()
+          })
+        })
+      })
+      .then(function () {
+        console.log(chalk.green.bold('✓ Files copied to build directory!'))
+      })
+      .catch(function (err) {
+        if (err.length === 1 && err[0].code === 'ENOENT') {
+          // no build directory, don't bother doing anything
+        } else {
+          throw err
+        }
+      })
+  })
   .catch(function (err) {
-  if (err.length === 1 && err[0].code === 'ENOENT') {
-      // no build directory, don't bother doing anything
-  } else {
     throw err
-  }
-})
-})
-.catch(function (err) {
-  throw err
-})
+  })
